89d9ba16a378a2ed497451532e895ebe10e830c5
dparise <parisedj@umich.edu>
2013-12-02 17:46:08 -0500

    working on members page, layout in place, need to get photos, list of names, and ascensor working for headshot window

---
+++ public/js/members.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+
+/*
+	var headshots = $('#headshot').ascensor({direction:"x"});
+
+	$(".name li").click(function(event, index) {
+		headshots.trigger("scrollToStage", $(this).index());
+	});
+
+	$(".name li").hover(function(event, index) {
+		headshots.trigger("scrollToStage", $(this).index());
+	});                                               
+
+	headshots.on("scrollStart", function(event, floor){
+		$(".name li").removeClass("selected");
+		$(".name li:eq("+floor.to+")").addClass("selected");
+	});
+
+*/
+
+
+});
