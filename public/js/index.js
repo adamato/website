6b837ba01aad0ce6d22fc387715a6ae916dffddd
dparise <parisedj@umich.edu>
2014-01-13 22:29:25 -0500

    changes to the nav, and basic asthetics

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,13 @@
 
-var ascensor = $('#ascensor').ascensor({direction:"x",ascensorFloorName:['','RUSH','EVENTS'],loop:false,time:100,childType:'section'});
+var ascensor = $('#ascensor').ascensor({direction:"x",ascensorFloorName:['','RUSH','EVENTS'],loop:false,time:400,childType:'section'});
 
-$("#nav-bar div").click(function(event, index) {
-	ascensor.trigger("scrollToStage", $(this).index()-1);
+$('#nav-bar div').click(function(event, index) {
+	ascensor.trigger('scrollToStage', $(this).index()-1);
 });
 
 $(".vendor").fitVids();
 
-ascensor.on("scrollStart", function(event, floor){
-	$("#nav-bar div").removeClass("selected");
-	$("#nav-bar div:eq("+floor.to+")").addClass("selected");
+ascensor.on('scrollStart', function(event, floor){
+	$('#nav-bar div').removeClass('selected');
+	$('#nav-bar div:eq('+floor.to+')').addClass('selected nav-active');
 });
