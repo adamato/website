8ff3c19bef364653cc8bab3b317a54ad8ef2cca9
dparise <parisedj@umich.edu>
2014-01-18 10:58:36 -0500

    realtime chat

--- server.js
+++ server.js
@@ -1,59 +1,39 @@
 var express = require('express'),
-	// mongo = require('mongoose'),
-	formidable = require('formidable');
-var app = express();
-// mongo.connect('mongodb://localhost/interestInfo');
+	db = require('./db.js'),
+	http = require('http'),
+	clients = {};
+var app = express(), 
+	server = require('http').createServer(app), 
+	io = require('socket.io').listen(server);
 
+app.listen(9900,function(){
+	console.log('KTP web server listening on port 9900!');
+});
+
+app.set('views','views');
+app.set('view engine', 'jade');
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(express.cookieParser('dirty secrets'));
+app.use(express.session());
 app.use(express.static('public'));
 
-/*/
-*	serves http://0.0.0.0:9900/
-*	will serve http://kappathetapi.com/
-/*/
 app.get('/',function(req,res){
 	res.send('public/index.html');
 });
 
+io.sockets.on('connection', function (socket) { 
+		
+	clients[socket.id] = socket;
 
-/*/	Database init for interest form
-/*/
-/*var rushSchema = new mongo.Schema({
-	Name: String,
-	Email: String,
-	Year: String
+	socket.on('chat', function (data) {
+		app.render('post',{name:data.user,time:new Date(),text:data.text}, function (err,html) {
+			if(err) console.log(err);
+			socket.broadcast.emit('chatter',html);
+		});
+	});
+
 });
 
-var Rush = mongo.model('Rush', rushSchema);*/
-/*/
-*	process form input and store in database
-/*/
-/*app.post('/interest',function(req,res){
-	console.log('interested');
-	console.log(req.body);
-	var rush = new Rush({
-		Name: req.body.name,
-		Email: req.body.email,
-		Year: req.body.year
-	});
-	rush.save(function(err){
-		if(err){
-			console.log(err);
-			res.send('An error has occured processing the form');
-			return;
-		}
-		res.send(true);
-		return;
-	});
-});*/
-
-/*/
-*	tell our server to 'listen' on the given port
-*	this allows us to talk to serve requests on port 9900 while localhosting
-/*/
-app.listen(9900,function(){
-	console.log('KTP web server listening on port 9900!');
-});
