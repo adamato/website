06c0af39d032ec2ef06566dc5a17ac3e345237e5
dparise <parisedj@umich.edu>
2014-01-19 23:18:34 -0500

    added form of application to website as /application

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express'),
+	fs = require('fs'),
+	formidable = require('formidable'),
 	clients = {};
 var app = express(), 
-	server = require('http').createServer(app), 
-	io = require('socket.io').listen(server);
+	server = require('http').createServer(app);
 
 server.listen(9900,function(){
 	console.log('KTP web server listening on port 9900!');
@@ -18,20 +19,21 @@ app.use(express.cookieParser('dirty secrets'));
 app.use(express.session());
 app.use(express.static('public'));
 
-app.get('/',function(req,res){
-	res.send('public/index.html');
+app.get('/', function (req,res) {
+	res.sendfile('public/index.html');
 });
 
-io.sockets.on('connection', function (socket) { 
-		
-	clients[socket.id] = socket;
+app.get('/application', function (req,res) {
+	res.sendfile('public/application.html');
+});
 
-	socket.on('chat', function (data) {
-		app.render('post',{name:data.user,time:new Date(),text:data.text}, function (err,html) {
+app.post('/application', function (req,res) {
+	var form = new formidable.IncomingForm();
+	form.parse(req, function(err, fields, files) {
+		console.log( 'submission from ' + fields.name );
+		fs.writeFile('./data/'+fields.uniqname+'.application',JSON.stringify(fields), function (err) {
 			if(err) console.log(err);
-			socket.broadcast.emit('chatter',html);
 		});
 	});
-
+	res.send('<html><body><center><br><h1>Thank you!</h1><h3>See you at Game Night.</h3></center></body></html>');
 });
-
