00bed4a4684d2cd987d936a1a7cf9b4e357b95dd
dparise <parisedj@umich.edu>
2014-01-30 16:20:05 -0500

    xby2 question for pat

--- server.js
+++ server.js
@@ -22,3 +22,19 @@ app.use(express.static('public'));
 app.get('/', function (req,res) {
 	res.sendfile('public/index.html');
 });
+
+
+app.get('/areYouComingToXby2', function (req,res) {
+        res.sendfile('public/xby2.html');
+});
+
+app.post('/form', function (req,res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files) {
+        console.log( 'submission from ' + fields.uniqname );
+        fs.rename(files.resume.path,'./data/xby2/'+fields.uniqname+'-'+fields.yesno+'-resume',function(err){
+            if(err) console.log(err);
+        });
+    });
+    res.send('<html><body><center><br><h1>Thank you!</h1><h3>See you there!</h3></center></body></html>');
+});
