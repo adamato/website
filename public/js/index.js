833ca1f4430e5d183f1126c4a6cd7046c2e895a4
Dom Parise <domparise@gmail.com>
2014-01-30 20:35:38 -0500

    added comments and moved out the google analytics

--- public/js/index.js
+++ public/js/index.js
@@ -11,3 +11,13 @@ ascensor.on('scrollStart', function(event, floor){
 	$('#nav-bar div').removeClass('selected');
 	$('#nav-bar div:eq('+floor.to+')').addClass('selected nav-active');
 });
+
+// Google analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-46859810-1']);
+_gaq.push(['_trackPageview']);
+(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
