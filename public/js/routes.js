cb6f295e67edbe00bd781732f042be279be62020
Jeremy Wdowik <jjwdowik@umich.edu>
2014-07-25 03:01:34 -0400

    making text dynamic on home

--- public/js/routes.js
+++ public/js/routes.js
@@ -37,6 +37,37 @@ function showPage(section) {
 }
 
 
-$('.carousel').carousel({
-    interval: 4500
-});
+// $('.carousel').carousel({
+//     interval: 4500
+// });
+
+
+
+
+
+
+var firstBtn = ["Network", "Collaborate", "Friendship"];
+var secondBtn = ["Develop", "Code", "Innovation"];
+var thirdBtn = ["Socialize", "Consume", "Brotherhood"];
+
+function rotateTerm() {
+  var ct = $("#rotate1").data("term") || 0;
+  $("#rotate1").data("term", ct == firstBtn.length -1 ? 0 : ct + 1).text(firstBtn[ct]).fadeIn()
+              .delay(3000).fadeOut(200, rotateTerm);
+}
+$(rotateTerm);
+
+function rotateTerm2() {
+  var ct = $("#rotate2").data("term") || 0;
+  $("#rotate2").data("term", ct == secondBtn.length -1 ? 0 : ct + 1).text(secondBtn[ct]).fadeIn()
+              .delay(3000).fadeOut(200, rotateTerm2);
+}
+$(rotateTerm2);
+
+function rotateTerm3() {
+  var ct = $("#rotate3").data("term") || 0;
+  $("#rotate3").data("term", ct == thirdBtn.length -1 ? 0 : ct + 1).text(thirdBtn[ct]).fadeIn()
+              .delay(3000).fadeOut(200, rotateTerm3);
+}
+$(rotateTerm3);
+
