bab6d4b1b8b388923e7a7ed23eb020f0ebc79d9f
justinandersun <justinandersun@gmail.com>
2014-07-25 01:02:19 -0400

    I lied

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,25 +1,16 @@
 $(document).ready(function(){
 	$('#if_student').hide();
-	$('#if_company').hide();
-	$('#if_student-org').hide();
+	$('#if_representative').hide();
 	$('#closing').hide();
 	$('select[name="role"]').change(function(){
 		if ($('select[name="role"] option:selected').val() == 'student') {
 			$('#if_student').show();
-			$('#if_company').hide();
-			$('#if_student-org').hide();
+			$('#if_representative').hide();
 			$('#closing').show();
 		}
-		if ($('select[name="role"] option:selected').val() == 'company representative') {
+		if ($('select[name="role"] option:selected').val() == 'representative') {
 			$('#if_student').hide();
-			$('#if_company').show();
-			$('#if_student-org').hide();
-			$('#closing').show();
-		}
-		if ($('select[name="role"] option:selected').val() == 'student org representative') {
-			$('#if_student').hide();
-			$('#if_company').hide();
-			$('#if_student-org').show();
+			$('#if_representative').show();
 			$('#closing').show();
 		}
 	});
